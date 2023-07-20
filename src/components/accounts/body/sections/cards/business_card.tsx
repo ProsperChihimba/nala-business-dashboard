8f1b99acbcbfe844908a9a9aecaf2a6fe7c20907
ProsperChihimba <prosperchihimba@gmail.com>
2023-07-20 15:54:03 +0300

    Add: account business account box

--- src/components/accounts/body/sections/cards/business_card.tsx
+++ src/components/accounts/body/sections/cards/business_card.tsx
@@ -1,8 +1,140 @@
-import React from 'react'
+import {  Box, Flex, Spacer } from "@chakra-ui/react"
+import { Divider, Typography } from "antd";
+import { BsBank2 } from "react-icons/bs";
+import { GoInfo } from "react-icons/go";
+import { VscChevronRight } from "react-icons/vsc";
+import AppButton from "../../../../layout/button";
+import { FiChevronDown } from "react-icons/fi";
+import MerchantsChart from "../../../../layout/chart";
 
 const BusinessCardBox = () => {
+
+  const data = [
+      { registered__month: 1, registered__year: 2022, total: 18300196787, count: 289 }, 
+      { registered__month: 2, registered__year: 2023, total: 73614764207.28946, count: 489 },
+      { registered__month: 3, registered__year: 2022, total: 635000, count: 513 }, 
+      { registered__month: 4, registered__year: 2023, total: 18300196787.17048, count: 709 },
+      { registered__month: 5, registered__year: 2022, total: 108989000, count: 900 }, 
+      { registered__month: 6, registered__year: 2023, total: 7268083239.017362, count: 959 },
+  ]
+
+  const { Text } = Typography;
   return (
-    <div>BusinessCardBox</div>
+    <Box
+        width='48%'
+        fontFamily='IBM Plex Sans, sans-serif'
+        border='1px solid #D9D9D9'
+        borderRadius='20px'
+    >
+        
+        {/* header section */}
+        <Flex paddingTop='20px' paddingRight='20px' paddingLeft='20px' alignItems='center' mb='20px'>
+            {/* title */}
+            <BsBank2 size='20px' color='#000000' />
+
+            <Text
+                style={{
+                    fontFamily: 'IBM Plex Sans, sans-serif',
+                    fontSize: '14px',
+                    fontWeight: 500,
+                    color: '#454545',
+                    marginLeft: '8px',
+                    marginRight: '5px'
+                }}
+            >
+                Business account
+            </Text>
+
+            <GoInfo size='11px' color='#000000' />
+
+            <Spacer />
+
+            {/* view section */}
+            <Text
+                style={{
+                    fontFamily: 'IBM Plex Sans, sans-serif',
+                    fontSize: '13px',
+                    fontWeight: 400,
+                    color: '#073DFC',
+                    marginRight: '3px'
+                }}
+            >
+                View transactions
+            </Text>
+            <VscChevronRight size='20px' color='#073DFC' />
+        </Flex>
+
+
+        {/* card details */}
+        <Flex padding='0px 20px' alignItems='center'>
+
+            {/* balance */}
+            <Flex direction='column'>
+                <Text
+                    style={{
+                        fontFamily: 'IBM Plex Sans, sans-serif',
+                        fontSize: '11px',
+                        fontWeight: 500,
+                        color: '#454545',
+                    }}
+                >
+                    Balance
+                </Text>
+                <Text
+                    style={{
+                        fontFamily: 'IBM Plex Sans, sans-serif',
+                        fontSize: '25px',
+                        fontWeight: 500,
+                        color: '#454545',
+                    }}
+                >
+                    $100,000.00
+                </Text>
+            </Flex>
+        </Flex>
+
+        <Divider style={{ marginTop: '20px', marginBottom: '5px' }} />
+
+        {/* recent transactions */}
+
+        <Box paddingRight='20px' paddingLeft='20px' >
+            {/* title */}
+            <Flex justifyContent='space-between' marginTop='10px' alignItems='center'>
+              <Text
+                  style={{
+                      fontFamily: 'IBM Plex Sans, sans-serif',
+                      fontSize: '11px',
+                      fontWeight: 500,
+                      color: '#454545',
+                  }}
+              >
+                  Balance
+              </Text>
+
+              {/* month selector button */}
+              <AppButton
+                  label='Last 3 months'
+                  background='white'
+                  color='#000'
+                  icon={<FiChevronDown size='15px' style={{ marginLeft: 8 }} color='#000000' />}
+                  width='150px'
+                  borderColor='#DCDCDC'
+              />
+            </Flex>
+
+
+            <MerchantsChart
+              id='colorThree'
+              id_url="url(#colorThree)"
+              color='#60A9FF'
+              graph={data}
+              type='Completed Goals'
+            />
+
+
+        </Box>
+
+    </Box>
   )
 }
 
---
+++ src/components/layout/chart.tsx
@@ -0,0 +1,109 @@
+import { Box } from '@chakra-ui/react'
+import {
+    ResponsiveContainer,
+    AreaChart,
+    XAxis,
+    YAxis,
+    Area,
+    CartesianGrid,
+} from "recharts";
+
+interface Props {
+    id: string;
+    id_url: string;
+    color: string;
+    type: string;
+    graph: {
+        registered__month: number;
+        count: number;
+        total: number;
+        registered__year: number;
+    }[];
+}
+
+interface Data {
+    registered__month: number;
+    count: number;
+    total: number;
+    registered__year: number;
+}
+
+const MerchantsChart = ({id, id_url, color, type, graph} : Props) => {
+
+
+        const monthNames = [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec"
+        ];
+
+        function compareData(a: Data, b: Data) {
+            if (a.registered__year !== b.registered__year) {
+              return a.registered__year - b.registered__year;
+            }
+            return a.registered__month - b.registered__month;
+        }
+
+        graph.sort(compareData);
+          
+          // Sort the data array
+        // data.sort(compareData);
+
+        const newData = graph.map(subarr => {
+            const monthName = monthNames[subarr.registered__month - 1];
+            return {registered__month: monthName, count: subarr.count, total: subarr.total, registered__year: subarr.registered__year};
+        });
+
+        // number formater
+        const formatter = new Intl.NumberFormat('en-US', {
+            useGrouping: true,
+            minimumFractionDigits: 0
+    });
+        
+    return (
+        <Box h='144px' marginLeft="-30px">
+            {/* chart */}
+            <ResponsiveContainer width="100%" height='100%'>
+                <AreaChart data={newData}>
+                <defs>
+                    <linearGradient id={id} x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="50%" stopColor={color} stopOpacity={0.4} />
+                        <stop offset="100%" stopColor={color} stopOpacity={0.05} />
+                    </linearGradient>
+                </defs>
+        
+                <Area type="monotone" dataKey="count" stroke={color} fill={id_url}/>
+        
+                <XAxis
+                    dataKey="registered__month"
+                    axisLine={false}
+                    tickLine={false}
+                    tick={{ fontSize: 10 }} 
+                />
+        
+                <YAxis
+                    dataKey='count'
+                    axisLine={false}
+                    tickLine={false}
+                    tickCount={8}
+                    tickFormatter={(number) => {return formatter.format(number)}}
+                    tick={{ fontSize: 10 }}
+                />
+        
+                <CartesianGrid opacity={0.1} vertical={false} />
+                </AreaChart>
+            </ResponsiveContainer>
+        </Box>
+    )
+}
+
+export default MerchantsChart
