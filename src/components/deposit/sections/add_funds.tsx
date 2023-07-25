edd8dbbe087856058e2884c8e950914e2a0e5800
ProsperChihimba <prosperchihimba@gmail.com>
2023-07-25 21:11:35 +0300

    Add: deposit page add funds

--- src/components/deposit/sections/add_funds.tsx
+++ src/components/deposit/sections/add_funds.tsx
@@ -1,6 +1,9 @@
 import { Flex } from '@chakra-ui/react'
 import DepositHeading from './utils/heading'
 import { Typography } from 'antd';
+import DepositInput from './utils/input';
+import { FiChevronDown } from 'react-icons/fi';
+import AppButton from '../../layout/button';
 
 const AddFunds = () => {
 
@@ -21,6 +24,50 @@ const AddFunds = () => {
         >
             Deposit of funds takes  up to <span style={{ fontWeight: '500' }}>3 business days</span>
         </Text>
+
+
+        {/* inputs */}
+        <DepositInput 
+            title='From'
+            value=''
+            placeholder='Account'
+            isReadOnly={true}
+            percentage='10%'
+            marginBottom='0px'
+            marginTop='35px'
+            rightElement={<FiChevronDown size='15px' style={{ marginLeft: 8 }} color='#000000' />}
+        />
+
+        <DepositInput 
+            title='To'
+            value='Nala business account'
+            placeholder='Account'
+            isReadOnly={true}
+            percentage='20%'
+            marginBottom='40px'
+            marginTop='20px'
+            rightElement={
+                <Text
+                    style={{
+                        fontFamily: 'IBM Plex Sans, sans-serif',
+                        fontSize: '15px',
+                        fontWeight: 400,
+                        color: '#000',
+                    }}
+                >
+                    ***** 3102
+                </Text>
+            }
+        />
+
+        {/* button */}
+        <AppButton
+            label='Next'
+            background='#073DFC'
+            color='#fff'
+            width='160px'
+            borderColor='#073DFC'
+        />
     </Flex>
   )
 }
---
+++ src/components/deposit/sections/utils/input.tsx
@@ -0,0 +1,67 @@
+import { Flex, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { Typography } from 'antd';
+import { ReactElement } from 'react';
+
+interface InputProps  {
+    title: string;
+    value: string;
+    placeholder: string;
+    isReadOnly: boolean;
+    rightElement?: ReactElement;
+    percentage? : string;
+    marginTop: string,
+    marginBottom: string,
+}
+
+const DepositInput = ({title, value, placeholder, isReadOnly, rightElement, percentage, marginBottom, marginTop} : InputProps) => {
+
+    const { Text } = Typography;
+  return (
+    <Flex
+        direction='column'
+        mt={marginTop}
+        mb={marginBottom}
+    >
+
+        {/* input title */}
+        <Text
+            style={{
+                fontFamily: 'IBM Plex Sans, sans-serif',
+                fontSize: '18px',
+                fontWeight: 500,
+                color: '#000',
+                marginBottom: '5px',
+            }}
+        >
+            {title}
+        </Text>
+        <InputGroup width='600px'>
+            <Input
+                pl='25px'
+                placeholder={placeholder}
+                height='50px'
+                borderColor='#D9D9D9'
+                background='white'
+                focusBorderColor='#073DFC'
+                borderRadius='10px'
+                fontFamily='IBM Plex Sans, sans-serif'
+                fontSize='15px'
+                fontWeight= '400'
+                color='#000'
+                value={value}
+                isReadOnly={isReadOnly}
+                _placeholder={{
+                    fontFamily: 'IBM Plex Sans, sans-serif',
+                    color: '#9A9A9A',
+                    fontSize: '15px',
+                }}
+            />
+            <InputRightElement width={percentage} pt='12px'>
+                {rightElement}
+            </InputRightElement>
+        </InputGroup>
+    </Flex>
+  )
+}
+
+export default DepositInput
