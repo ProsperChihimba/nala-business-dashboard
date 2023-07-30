c31aea4b840f3588287c2f0f6d5adcd642bc62c9
ProsperChihimba <prosperchihimba@gmail.com>
2023-07-30 04:27:09 +0300

    Add: steps in signup page

--- src/components/authentication/signup/steps.tsx
+++ src/components/authentication/signup/steps.tsx
@@ -1,10 +1,25 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, Step, StepDescription, StepIndicator, StepSeparator, StepStatus, StepTitle, Stepper, useSteps } from '@chakra-ui/react'
 import { Image } from 'antd'
 
 // assets
 import logo from '../../../assets/Logo.svg';
+import { MdBusiness } from 'react-icons/md';
+
+const steps = [
+  { title: 'Business details', description: 'Answer few questions about your business to open your account' },
+  { title: 'Company website', description: 'In order to understand more about your business we need your website address' },
+  { title: 'Company size', description: 'As we’re setting up your business account it’s important for Nala to know your company size' },
+  { title: 'Company revenue', description: 'As our clients it’s import for Nala to know average business revenue' },
+  { title: 'Account password', description: 'Security is our first priority at NALA so it’s for your account to have a password' },
+]
 
 const SignupSteps = () => {
+
+  const { activeStep } = useSteps({
+    index: 0,
+    count: steps.length,
+  })
+
   return (
     <Box
         backgroundColor='#F9F9F9' 
@@ -26,6 +41,28 @@ const SignupSteps = () => {
             preview={false}
         />
       </Flex>
+
+      {/* steps */}
+      <Stepper index={activeStep} orientation='vertical' height='400px' gap='0' w='80%'>
+      {steps.map((step, index) => (
+        <Step key={index}>
+          <StepIndicator border='0px solid #DCDCDC' bg='white' borderRadius='10px'>
+            <StepStatus
+              complete={<MdBusiness />}
+              incomplete={<MdBusiness />}
+              active={<MdBusiness />}
+            />
+          </StepIndicator>
+
+          <Box flexShrink='0' w='100%'>
+            <StepTitle style={{fontWeight: 400, fontSize: '16px'}}>{step.title}</StepTitle>
+            <StepDescription style={{fontWeight: 400, fontSize: '12px', color: '#9A9A9A'}}>{step.description}</StepDescription>
+          </Box>
+
+          <StepSeparator />
+        </Step>
+      ))}
+    </Stepper>
     </Box>
   )
 }
